a4632d95fb183177522d8bb6ca45194acd519878
HsMehrvi <contact.hshahid@gmail.com>
2026-06-18 17:01:08 +0500

    Fix: remove fake-backend, fix splash screen logo path

--- Vue/src/App.vue
+++ Vue/src/App.vue
@@ -9,11 +9,7 @@ import { RouterView } from 'vue-router';
 import BackToTop from '@/components/BackToTop.vue';
 import AOS from 'aos';
 
-import configureFakeBackend from '@/helpers/fake-backend';
-
 import { useLayoutStore } from '@/stores/layout';
-
-configureFakeBackend();
 const useLayout = useLayoutStore();
 useLayout.setTheme(useLayout.theme);
 
